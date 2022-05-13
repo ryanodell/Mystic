06c63ff86e1a7838c37f2cb40a252a14064a3da0
Meeks Meeks <blurubberduck87@gmail.com>
2022-05-13 16:23:14 -0700

    Passing colors proof of concept

---
+++ MysticEngineTK/Implementations/PassingInColorExample.cs
@@ -0,0 +1,99 @@
+﻿using MysticEngineTK.Core;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace MysticEngineTK {
+    internal class PassingInColorExample : Game {
+        public PassingInColorExample(int initialWindowWidth, int initialWindowHeight, string initialWindowTitle) : base(initialWindowWidth, initialWindowHeight, initialWindowTitle) { }
+
+        private readonly float[] _vertices = {
+            //Positions         //Colors
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,    // Bottom-left vertex - Red
+             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,    // Bottom-right vertex - Green
+             0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // Top vertex - Blue
+        };
+
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+
+        private int _shaderHandle;
+
+        protected override void Initalize() {
+
+        }
+
+        protected override void LoadContent() {
+            string vertexShader = @"
+            #version 330 core
+            layout (location = 0) in vec3 aPosition;
+            layout (location = 1) in vec3 aColor;
+            out vec4 vertexColor;
+
+            void main() 
+            {
+                vertexColor = vec4(aColor.rgb, 1.0);
+            	gl_Position = vec4(aPosition.xyz, 1.0);
+            }";
+
+            string fragementShader = @"
+            #version 330 core
+            out vec4 color;
+            in vec4 vertexColor;
+            void main() 
+            {
+            	color = vertexColor;
+            }";
+
+            int vertexShaderId = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShaderId, vertexShader);
+            GL.CompileShader(vertexShaderId);
+            GL.GetShader(vertexShaderId, ShaderParameter.CompileStatus, out var vertesShaderCompilationCode);
+            if (vertesShaderCompilationCode != (int)All.True) {
+                Console.WriteLine(GL.GetShaderInfoLog(vertexShaderId));
+            }
+
+            int fragmentShaderId = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShaderId, fragementShader);
+            GL.CompileShader(fragmentShaderId);
+            GL.GetShader(fragmentShaderId, ShaderParameter.CompileStatus, out var fragmentShaderCompilationCode);
+            if (fragmentShaderCompilationCode != (int)All.True) {
+                Console.WriteLine(GL.GetShaderInfoLog(fragmentShaderId));
+            }
+            _shaderHandle = GL.CreateProgram();
+            GL.AttachShader(_shaderHandle, vertexShaderId);
+            GL.AttachShader(_shaderHandle, fragmentShaderId);
+
+            GL.LinkProgram(_shaderHandle);
+            GL.DetachShader(_shaderHandle, vertexShaderId);
+            GL.DetachShader(_shaderHandle, fragmentShaderId);
+            GL.DeleteShader(vertexShaderId);
+            GL.DeleteShader(fragmentShaderId);
+
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+        }
+        protected override void Update(GameTime gameTime) {
+
+        }
+
+        protected override void Render() {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color4.CornflowerBlue);
+            GL.UseProgram(_shaderHandle);
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+        }
+
+    }
+}
