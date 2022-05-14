cfa0bc588140b23185d5f8f86aa6434f0c79f4db
Meeks Meeks <blurubberduck87@gmail.com>
2022-05-14 10:36:10 -0700

    fixed calling layout

--- MysticEngineTK/Implementations/ManualShader.cs
+++ MysticEngineTK/Implementations/ManualShader.cs
@@ -33,7 +33,7 @@ namespace MysticEngineTK {
 
             string fragementShader = @"
             #version 330 core
-            layout(location = 0) out vec4 color;
+            out vec4 color;
             void main() 
             {
             	color = vec4(1.0, 0.0, 0.0, 1.0);
