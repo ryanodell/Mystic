05f5c21edb43595d0add3a3c8c3078d29ac10977
Meeks Meeks <blurubberduck87@gmail.com>
2022-05-15 05:27:17 -0700

    Added texture2d class

---
+++ MysticEngineTK.Core/Rendering/Texture2D.cs
@@ -0,0 +1,39 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace MysticEngineTK.Core.Rendering {
+    public class Texture2D {
+        public int Handle;
+
+        public Texture2D(int handle) {
+            Handle = handle;
+        }
+
+        public static Texture2D LoadFromFile(string path) {
+            int handle = GL.GenTexture();
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, handle);
+            using(var image = new Bitmap(path)) {
+                image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                var data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, 
+                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            }
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+
+            return new Texture2D(handle);
+        }
+
+        public void Use() {
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+        }
+    }
+}
