9ac7c6b3d3f53a826104eca4d891f32d525bd85f
Meeks Meeks <blurubberduck87@gmail.com>
2022-05-23 10:38:04 -0700

    Implemented Colors with texture

---
+++ MysticEngineTK/Implementations/TextureWithColor.cs
@@ -0,0 +1,70 @@
+﻿using MysticEngineTK.Core;
+using MysticEngineTK.Core.Management;
+using MysticEngineTK.Core.Rendering;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace MysticEngineTK {
+    internal class TextureWithColor : Game {
+        public TextureWithColor(int initialWindowWidth, int initialWindowHeight, string initialWindowTitle) : base(initialWindowWidth, initialWindowHeight, initialWindowTitle) { }
+
+        private readonly float[] _vertices = {
+            // Position         Texture coordinates
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, // top right
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f  // top left
+        };
+
+        private readonly uint[] _indices = {
+            0, 1, 3,
+            1, 2, 3
+        };
+
+        private VertexBuffer _vertexBuffer;
+        private VertexArray _vertexArray;
+        private IndexBuffer _indexBuffer;
+
+        Shader _shader;
+        Texture2D _texture;
+
+        protected override void Initalize() {
+
+        }
+
+        protected override void LoadContent() {
+            _vertexArray = new VertexArray();
+            _vertexBuffer = new(_vertices);
+            BufferLayout bufferLayout = new BufferLayout();
+            //Positions
+            bufferLayout.Add<float>(3);
+            //Texture Coordinates
+            bufferLayout.Add<float>(2);
+            //Colors:
+            bufferLayout.Add<float>(3);
+
+            _vertexArray.AddBuffer(_vertexBuffer, bufferLayout);
+            _shader = new(Shader.ParseShader("Resources/Shaders/TextureWithColor.glsl"));
+            bool result = _shader.CompileShader();
+            if (result) {
+                _shader.Use();
+            }
+            _indexBuffer = new(_indices);
+            _texture = ResourceManager.Instance.LoadTexture("Resources/Textures/container.png");
+            _texture.Use();
+
+        }
+        protected override void Update(GameTime gameTime) {
+
+        }
+
+        protected override void Render() {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color4.CornflowerBlue);
+            _vertexArray.Bind();
+            _shader?.Use();
+            _indexBuffer.Bind();
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+        }
+    }
+}
--- MysticEngineTK/Program.cs
+++ MysticEngineTK/Program.cs
@@ -1,12 +1,11 @@
 ﻿using MysticEngineTK.Core;
-using System.IO;
 
 namespace MysticEngineTK {
     public class Program
     {        
         public static void Main(string[] args)
         {            
-            Game game = new BufferAbstraction(800, 600, "Mystic Engine");
+            Game game = new TextureWithColor(800, 600, "Mystic Engine");
             game.Run();
         }
     }
